a456392e430b7a200c9b260cee41e85cd67c58e9
allstar94 <alsltar94@gmail.com>
2021-10-15 09:01:03 +0900

    1015

--- JavaTest/src/test/Example_try_catch.java
+++ JavaTest/src/test/Example_try_catch.java
@@ -3,8 +3,20 @@ package test;
 public class Example_try_catch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num1 = 10, num2 = 0;
+		double array[] = new double[10];
+		try {
+			array[11] = num1/num2;
+		}catch(ArithmeticException e) {
+			System.out.println("0으로 나눗셈을 하면 안됩니다.");
+			System.out.println("에러 내용 : " + e.toString());
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열의 길이를 벗어났습니다.");
+			System.out.println("에러 내용 :" + e.toString());
+		}finally {
+			System.out.println("여기는 반드시 실행해야하는 부분입니다.");
+		}
+		
 	}
 
 }
